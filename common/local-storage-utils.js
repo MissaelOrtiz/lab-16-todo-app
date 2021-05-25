d92ff5ac1746e197beee7aa3627ee517764ffab1
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-25 16:16:03 -0700

    added setUser function

--- common/local-storage-utils.js
+++ common/local-storage-utils.js
@@ -2,3 +2,8 @@ export function getUser(username) {
     const stringyuser = localStorage.getItem(username);
     return JSON.parse(stringyuser);
 }
+
+export function setUser(user) {
+    const stringyUser = JSON.stringyuser(user);
+    localStorage.setItem(user.username, stringyUser);
+}
