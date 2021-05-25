057bcf0bfbf07bdd521f97b78989262ea5b6b7eb
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-25 16:22:58 -0700

    added createUser function

--- common/local-storage-utils.js
+++ common/local-storage-utils.js
@@ -7,3 +7,17 @@ export function setUser(user) {
     const stringyUser = JSON.stringyuser(user);
     localStorage.setItem(user.username, stringyUser);
 }
+
+export function createUser(username, password) {
+    if (localStorage.getItem(username)) {
+        alert('This username already exists. Please choose another username.');
+    } else {
+        const newUser = {
+            username: username,
+            password: password,
+            todos: []
+        };
+        setUser(newUser);
+        // ADD IN LOG IN FUNCTION WHEN WRITTEN HERE
+    }
+}
